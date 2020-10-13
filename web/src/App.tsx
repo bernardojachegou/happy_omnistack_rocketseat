8e7cd91e77bcbb0cc139aae56c7f1ae9eaa581c3
Michel Bernardo <bernardoenglish@outlook.com>
2020-10-12 20:48:27 -0400

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+function App() {
+  return (
+    <div id="page-landing">
+      <h1>Rocket</h1>
+    </div>
+
+  );
+}
+
+export default App;
