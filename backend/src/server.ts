58b2653eec8b0f317cb3b88b8a53680438dc1745
Michel Bernardo <bernardoenglish@outlook.com>
2020-10-14 07:45:09 -0400

    Happy API prepared

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,23 @@
 import express from 'express';
+import path from 'path';
+import cors from 'cors';
+
+import 'express-async-errors';
+
 import './database/connection';
 
 import routes from './routes';
+import errorHandler from './errors/handler';
+
 
 const app = express();
 
+// Cors used to allow any frontend to use the API;
+app.use(cors());
+
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333);
