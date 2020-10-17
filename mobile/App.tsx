30f3371fc68413afb1bd29c3c5ec485016ea76cb
Michel Bernardo <bernardoenglish@outlook.com>
2020-10-17 09:10:15 -0400

    adding splash screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,6 +5,7 @@ import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-g
 import Routes from './src/routes';
 
 export default function App() {
+
   const [fontsLoaded] = useFonts({
     Nunito_600SemiBold,
     Nunito_700Bold,
