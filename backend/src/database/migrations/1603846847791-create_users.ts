8678d2c730b2a0f2b6eaef4a59403afa9627a836
Michel Bernardo <bernardoenglish@outlook.com>
2020-10-27 21:18:39 -0400

    migration to table users created

---
+++ backend/src/database/migrations/1603846847791-create_users.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createUsers1603846847791 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'users',
+            columns: [
+                {
+                    name: 'user_id',
+                    type: 'integer',
+                    unsigned: true,
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment'
+                },
+                {
+                    name: 'name',
+                    type: 'varchar'
+                },
+                {
+                    name: 'email',
+                    type: 'varchar'
+                },
+                {
+                    name: 'password',
+                    type: 'varchar'
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
+
+    }
+
+}
